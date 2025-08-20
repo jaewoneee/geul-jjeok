5245064bcdc4b5596850b686c646d86489759304
jaewoneee <jaewon810@naver.com>
2025-08-20 16:40:07 +0900

    feat: 라우팅 구조(wip)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,63 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Session } from "@supabase/supabase-js";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const segments = useSegments();
+  const router = useRouter();
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  useEffect(() => {
+    // 초기 세션 확인
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      console.log({ session });
+      setSession(session);
+      setIsLoading(false);
+    });
+
+    // 세션 변경 감지
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+      setIsLoading(false);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    const inAuthGroup = segments[0] === "(auth)";
+    const inTabsGroup = segments[0] === "(tabs)";
+
+    if (session && inAuthGroup) {
+      console.log(1);
+      // 로그인된 상태에서 auth 페이지에 있으면 메인으로 이동
+      router.replace("/(tabs)");
+    } else if (!session && inTabsGroup) {
+      console.log(2);
+      // 로그인되지 않은 상태에서 tabs 페이지에 있으면 로그인으로 이동
+      router.replace("/(auth)");
+    } else {
+      console.log(3);
+      router.replace("/(tabs)");
+    }
+  }, [session, segments, isLoading]);
+
+  // if (isLoading) {
+  //   // 로딩 스피너 컴포넌트
+  //   return <LoadingScreen />;
+  // }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(auth)" />
+      {/* <Stack.Screen name="models" options={{ presentation: "modal" }} /> */}
+    </Stack>
   );
 }
