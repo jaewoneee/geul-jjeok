fb1ac318ccb51b3bed8d7609a44ea29949e69955
jaewoneee <jaewon810@naver.com>
2025-08-22 14:38:40 +0900

    feat: 로그아웃 기능

--- app/(main)/setting/index.tsx
+++ app/(main)/setting/index.tsx
@@ -1,12 +1,25 @@
 import { ThemedButton } from "@/components/ThemedButton";
 import { ThemedText } from "@/components/ThemedText";
+import { signOut } from "@/lib/auth";
+import { useRouter } from "expo-router";
+import { Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SettingScreen() {
+  const router = useRouter();
+
+  const onPressLogout = async () => {
+    try {
+      await signOut();
+      router.replace("/(auth)");
+    } catch (e: any) {
+      Alert.alert("로그아웃 실패", e.message ?? "잠시 후 다시 시도해 주세요.");
+    }
+  };
   return (
     <SafeAreaView>
       <ThemedText>Setting</ThemedText>
-      <ThemedButton label="Logout" />
+      <ThemedButton label="Logout" onPress={onPressLogout} />
     </SafeAreaView>
   );
 }
